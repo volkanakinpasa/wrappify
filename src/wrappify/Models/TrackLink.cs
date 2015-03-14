fd67988290d382e85befd842562797a5156a82a4
volkanakinpasa <volkanakintr@gmail.com>
2015-03-14 12:57:38 +0200

    Tracklink.cs and trackresult.cs added

---
+++ src/wrappify/Models/TrackLink.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace wrappify.Models
+{
+    [JsonObject]
+    public class TrackLink
+    {
+        [JsonProperty(PropertyName = "external_urls")]
+        public Dictionary<string, string> ExternalUrls { get; set; }
+
+        [JsonProperty(PropertyName = "href")]
+        public string Href { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "uri")]
+        public string Uri { get; set; }
+    }
+}
---
+++ src/wrappify/Models/TrackResult.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace wrappify.Models
+{
+    public class TrackResult
+    {
+        [JsonProperty(PropertyName = "tracks")]
+        public Track[] Tracks { get; set; }  
+    }
+}
